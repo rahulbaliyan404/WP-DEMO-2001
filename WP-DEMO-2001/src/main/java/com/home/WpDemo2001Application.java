8f5e28f8ec5f1dced221ddd96eeefcc2ab8f29f8
rahulbaliyan <rahulbaliyan404@gmail.com>
2021-02-22 12:26:19 +0530

    change

--- WP-DEMO-2001/src/main/java/com/home/WpDemo2001Application.java
+++ WP-DEMO-2001/src/main/java/com/home/WpDemo2001Application.java
@@ -2,8 +2,9 @@ package com.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class WpDemo2001Application {
 
